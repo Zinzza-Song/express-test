af8f8156c911d635d0036d04a3bfe7e93b4c0e94
Han-Bin Song <thdgksq@naver.com>
2023-04-24 13:11:06 +0900

    swagger 적용2

--- src/swagger/index.js
+++ src/swagger/index.js
@@ -3,6 +3,36 @@ import defaultSwagger from "./defaultSwagger";
 
 console.log(Swaggers);
 
+const { paths } = Object.values(Swaggers).reduce(
+  (acc, apis) => {
+    console.log("apis", apis);
+    const APIs = Object.values(apis).map((api) => {
+      console.log(api);
+      return { ...api };
+    });
+    APIs.forEach((api) => {
+      const key = Object.keys(api)[0];
+      console.log(key);
+      if (!acc.paths[key]) {
+        acc.paths = {
+          ...acc.paths,
+          ...api,
+        };
+      } else {
+        acc.paths[key] = {
+          ...acc.paths[key],
+          ...api[key],
+        };
+      }
+    });
+    console.log(acc);
+    console.log(APIs);
+
+    return acc;
+  },
+  { paths: {} }
+);
+
 // 스웨거에 등록한 json
 export const swaggerDocs = {
   ...defaultSwagger,
